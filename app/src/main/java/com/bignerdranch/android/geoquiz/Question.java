e4dc3436dd40c0d6db13dafb319d6eae3915e22d
mbeevor <mbeevor@gmail.com>
2018-02-06 19:13:52 +0000

    Chapter Two - Listing 2.11

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Matthew on 06/02/2018.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+}
